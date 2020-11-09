a3e7c368fb343c8a70ee4c8442ec50322c28e31f
martinballa <ballamartin95@gmail.com>
2020-11-09 12:16:45 +0000

    Added a new runner to run on apocrita

--- Minecraft/launchClient_apocrita.sh
+++ Minecraft/launchClient_apocrita.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-singularity exec /data/containers/test/xfce3.img xvfb-run -a -e /dev/stdout -s '-screen 0 640x480x16' bash launchClient.sh -port $1 -env > ../out.txt 2>&1
+singularity exec /data/containers/test/xfce3.img xvfb-run -a -e /dev/stdout -s '-screen 0 640x480x16' ./launchClient.sh -port $1 -env > ../out.txt 2>&1
