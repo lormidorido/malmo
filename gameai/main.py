3543cfc483a22ffacbe3ff9f662ad8d8162db4d7
martinballa <ballamartin95@gmail.com>
2020-11-10 14:17:35 +0000

    Moved utils to malmoenv/utils

--- gameai/main.py
+++ gameai/main.py
@@ -1,11 +1,9 @@
 import os, sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import time
-from MalmoEnv.utils.launcher import launch_minecraft
+from malmoenv.utils.launcher import launch_minecraft
 from gameai.utils.utils import parse_args, create_env
-from gameai.utils.symbolic_wrappers import MultiEntrySymbolicObs, SymbolicObs
-from gameai.utils.wrappers import ScreenCapturer, DownsampleObs
-
+from gameai.utils.symbolic_wrappers import SymbolicObs
 
 if __name__ == "__main__":
     args = parse_args()
