0650f39af249b4f8a082f6abfaaeac7e49635f4a
martinballa <ballamartin95@gmail.com>
2020-11-12 16:18:58 +0000

    Added the wrappers and some common function to the utils subpackage in malmoenv

---
+++ MalmoEnv/malmoenv/utils/utils.py
@@ -0,0 +1,48 @@
+import argparse, os
+import malmoenv
+from pathlib import Path
+# from gameai.utils.wrappers import DownsampleObs
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='malmoenv arguments')
+    parser.add_argument('--mission', type=str, default='../MalmoEnv/missions/mobchase_single_agent.xml',
+                        help='the mission xml')
+    parser.add_argument('--port', type=int, default=10000, help='the mission server port')
+    parser.add_argument('--server', type=str, default='127.0.0.1', help='the mission server DNS or IP address')
+    # todo next 2 arguments can be removed
+    # https://github.com/elpollouk/malmo/blob/elpollouk/MultiAgentEnv/MalmoEnv/rllib_train.py
+    parser.add_argument('--port2', type=int, default=None,
+                        help="(Multi-agent) role N's mission port. Defaults to server port.")
+    parser.add_argument('--server2', type=str, default=None, help="(Multi-agent) role N's server DNS or IP")
+    parser.add_argument('--episodes', type=int, default=1, help='the number of resets to perform - default is 1')
+    parser.add_argument('--episode', type=int, default=0, help='the start episode - default is 0')
+    parser.add_argument('--role', type=int, default=0, help='the agent role - defaults to 0')
+    parser.add_argument('--episodemaxsteps', type=int, default=0, help='max number of steps per episode')
+    # parser.add_argument('--saveimagesteps', type=int, default=0, help='save an image every N steps')
+    parser.add_argument('--resync', type=int, default=0, help='exit and re-sync every N resets'
+                                                              ' - default is 0 meaning never.')
+    parser.add_argument('--experimentUniqueId', type=str, default='test1', help="the experiment's unique id.")
+    args = parser.parse_args()
+
+    if args.server2 is None:
+        args.server2 = args.server
+    # Better to use absolute path for XML files in case working directory would change
+    args.mission = os.path.realpath(args.mission)
+
+    return args
+
+def create_env(args):
+    xml = Path(args.mission).read_text()
+    env = malmoenv.make()
+    print(f"create env listening on port {args.port}")
+    env.init(xml, args.port,
+             server=args.server,
+             server2=args.server2, port2=args.port2,
+             role=args.role,
+             exp_uid=args.experimentUniqueId,
+             episode=args.episode, resync=args.resync,
+             reshape=True)
+    env.reward_range = (-float('inf'), float('inf'))
+    # env = DownsampleObs(env, shape=tuple((84, 84)))
+    # env = MultiEntrySymbolicObs(env)
+    return env
---
+++ MalmoEnv/malmoenv/utils/wrappers.py
@@ -0,0 +1,132 @@
+from malmoenv.core import Env
+from gym.spaces import Box
+import cv2, gym
+import numpy as np
+import ffmpeg
+
+# Template for creating a Malmo wrapper
+class DummyWrapper(Env):
+    def __init__(self, env):
+        super(DummyWrapper, self).__init__(env)
+        self.env = env
+        self.observation_space = env.observation_space
+        self.action_space = env.action_space
+
+    def step(self, action):
+        obs, r, done, info = self.env.step(action)
+        obs = self.observation(obs)
+        return obs, r, done, info
+
+    def reset(self):
+        obs = self.env.reset()
+        return self.observation(obs)
+
+    def observation(self, obs):
+        return obs
+
+# Wrapper to convert observations to a desired shape
+class DownsampleObs(Env):
+    def __init__(self, env, shape):
+        super(DownsampleObs, self).__init__(env)
+        self.env = env
+        self.shape = shape # env.observation_space.shape[:2]
+        self.observation_space = Box(low=0, high=255, shape=self.shape + env.observation_space.shape[2:], dtype=np.uint8)
+        self.action_space = env.action_space
+
+    def step(self, action):
+        obs, r, done, info = self.env.step(action)
+        self.score += r
+        self.steps += 1
+        obs = self.observation(obs)
+        info = {"raw_info": info}
+        if done:
+            print(f"episode finished in {self.steps} and with score {self.score}")
+        return obs, r, done, info
+
+    def reset(self):
+        obs = self.env.reset()
+        self.steps = 0
+        self.score = 0
+        return self.observation(obs)
+
+    def observation(self, obs):
+        if obs is None:
+            print("obs is None")
+        else:
+            obs = cv2.resize(obs, self.shape[::-1], interpolation=cv2.INTER_AREA)
+            if obs.ndim == 2:
+                obs = np.expand_dims(obs, -1)
+            obs = obs / 255.0
+        return obs
+
+class ScreenCapturer(Env):
+    """
+    Should use this before modifying the observation space with other wrappers
+    - uses python-ffmpeg in the background
+
+    """
+    def __init__(self, env, format="gif", savepath="", name="malmo", size=None, accumulate_episodes=1):
+        super(ScreenCapturer, self).__init__(env)
+        self.env = env
+        self.observation_space = env.observation_space
+        self.action_space = env.action_space
+        self.savepath = savepath
+        self.fps = 6
+        self.name = name
+        self.in_size = env.observation_space.shape[:2]
+        self.out_size = size if (size != None) else env.observation_space.shape[:2]
+        if format not in ["gif", "mp4"]:
+            raise Exception(f"Provided format: [{format}] is currently not supported")
+        self.format = format
+        self.accumulate_episodes = accumulate_episodes # number of episodes to collect before creating a frame
+
+        # store each frame in an array
+        self.frames = []
+        self.episode = 0
+        self.total_reward = 0
+        self.total_length = 0
+
+    def step(self, action):
+        obs, r, done, info = self.env.step(action)
+        self.total_reward += r
+        self.total_length += 1
+        self.frames.append(self.process_frame(np.copy(obs)))
+        if done and self.episode % self.accumulate_episodes == 0:
+            self.save_gif()
+        return obs, r, done, info
+
+    def reset(self):
+        self.episode += 1
+        obs = self.env.reset()
+        self.frames.append(self.process_frame(obs))
+        return obs
+
+    def process_frame(self, frame):
+        # convert to np array
+        # frame = np.array(frame*255, dtype=np.uint8)
+        frame = cv2.rotate(frame, cv2.ROTATE_180)
+        return frame
+
+    def save_gif(self):
+        reward = self.total_reward / self.accumulate_episodes
+        length = self.total_length / self.accumulate_episodes
+        filename = f"{self.name}_e{self.episode}_l{length:.2f}_r{reward:.2f}.{self.format}"
+        process = (
+            ffmpeg
+            .input("pipe:", format='rawvideo', pix_fmt='rgb24', s=f"{self.in_size[1]}x{self.in_size[0]}")
+            .output(filename, s=f"{self.out_size[1]}x{self.out_size[0]}", sws_flags="neighbor", r=self.fps)
+            .overwrite_output()
+            .run_async(pipe_stdin=True)
+        )
+        for frame in self.frames:
+            process.stdin.write(
+                frame
+                .astype(np.uint8)
+                .tobytes()
+            )
+        process.stdin.close()
+        process.wait()
+
+        self.frames = []
+        self.total_reward = 0
+        self.total_length = 0
